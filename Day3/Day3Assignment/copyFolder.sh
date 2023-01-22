66225f9fb1019c6d85be6eb8a7231c38099134bf
RaimaChakraborty <raimachakraborty0715@gmail.com>
2023-01-22 21:07:43 +0530

    Add: Day3 Practice Problems and Assignments added

---
+++ Day3/Day3Assignment/copyFolder.sh
@@ -0,0 +1,12 @@
+for files in *.txt
+do
+        folderName=`echo $files | awk -F. '{print $1}'`;
+        printf "\n";
+        if [ -d $folderName ]
+        then
+                rm -r $folderName;
+        fi
+        mkdir $folderName;
+        mv $files $folderName/$files;
+	echo $folderName/$files
+done
